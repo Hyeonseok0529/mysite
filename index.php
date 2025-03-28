7bd76ba33915b8a70a9a209692d6f27c630055fc
Hyeonseok0529 <gustjr8293@naver.com>
2025-03-28 17:02:08 +0900

    백엔드쪽 공부중

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php 첫페이지</title>
+    <script>
+        location.replace('/vogue/');
+    </script>
+    
 </head>
 <body>
     <h1>나의 php 첫 페이지야!</h1>
